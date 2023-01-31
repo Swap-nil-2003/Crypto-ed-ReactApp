81b32efca9c49a51c8149e8ca381637aae9fe28d
Swapnil Aichbhaumik <swapnil.aichbhaumik.cse25@heritageit.edu.in>
2023-01-31 22:40:30 +0530

    Update Featured.jsx

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -4,27 +4,26 @@ import React from "react";
 const Featured = () => {
     return (
     <Box bgColor={"blackAlpha.500"} w={"full"} p={"20"}>
-        {/* <Text>Featured Section</Text> */}
         <Stack direction={["column", "row"]} h={"full"} alignItems={"center"}>
           <VStack w={"100vw"} alignItems={["center"]}  textAlign={"center"}>
             <>
               <i class="fa-solid fa-circle-check fa-4x f-icon"></i> 
-              <Heading class="feature-title">Easy to use.</Heading>
-              <p>So easy to use, even a layman could do it.</p>
+              <Heading class="feature-title">Easy to use</Heading>
+              <p>No need of any technical expertise</p>
             </>
           </VStack>
           <VStack w={"100vw"} alignItems={["center"]} textAlign={"center"}>
             <>
               <i class="fa-solid fa-bullseye fa-4x f-icon"></i>
-              <h3 class="feature-title">Elite Clientele</h3>
-              <p>We have all the real time data.</p>
+              <h3 class="feature-title">Real-Time Data</h3>
+              <p>We collect all the real-time data</p>
             </>            
           </VStack>
           <VStack w={"100vw"} alignItems={["center"]} textAlign={"center"}>
             <>
               <i class="fa-solid fa-heart fa-4x f-icon"></i>
-              <h3 class="feature-title">Guaranteed to work.</h3>
-              <p>Find the suitable coins and exchange rates</p>
+              <h3 class="feature-title">Guaranteed to work</h3>
+              <p>Find info about your favourite coins and exchange rates</p>
             </>            
           </VStack>
         </Stack>
