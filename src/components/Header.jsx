780a3606e419556f010f60cdee6710096e6de78b
Swapnil Aichbhaumik <swapnil.aichbhaumik.cse25@heritageit.edu.in>
2023-02-21 22:59:32 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return(
   <HStack p={"4"} shadow={"base"} bgColor={'blackAlpha.700'}>
-    <nav class="navbar navbar-expand-lg navbar-dark">
+    <nav class="navbar navbar-expand-sm navbar-dark">
         <button class="navbar-toggler" type="button" style={{ marginRight: 10 }} data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
