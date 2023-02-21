b73a06e30569cbdaf7c0060c29006493099fa1c6
Upasana-Roy <107022993+Upasana-Roy@users.noreply.github.com>
2023-02-21 22:26:32 +0530

    Update Header.jsx
    Toggle button in navbar added.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,21 +5,42 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return(
   <HStack p={"4"} shadow={"base"} bgColor={'blackAlpha.700'}>
-    <Button variant={"unstyled"} color={"white"}>
-        <Link to="/">Home</Link>
-    </Button>
-    <Button variant={"unstyled"} color={"white"}>
-        <Link to="/exchanges">Exchanges</Link>
-    </Button>
-    <Button variant={"unstyled"} color={"white"}>
-        <Link to="/coins">Coins</Link>
-    </Button>
-    <Button variant={"unstyled"} color={"white"}>
-        <Link to="/trending">Trending</Link>
-    </Button>
-    <Button variant={"unstyled"} color={"white"}>
-        <Link to="/featured">Featured</Link>
-    </Button>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <button class="navbar-toggler" type="button" style={{ marginRight: 10 }} data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <p class="navbar-brand">Crypto'ed</p>
+        
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+            <Button variant={"unstyled"} color={"white"} style={{ marginLeft: '1rem' }}>
+                <Link to="/">Home</Link>
+            </Button>
+            </li>
+            <li class="nav-item">
+            <Button variant={"unstyled"} color={"white"} style={{ marginLeft: '1rem' }} >
+                <Link to="/exchanges">Exchanges</Link>
+            </Button>
+            </li>
+            <li class="nav-item">
+            <Button variant={"unstyled"} color={"white"} style={{ marginLeft: '1rem' }}>
+                <Link to="/coins">Coins</Link>
+            </Button>
+            </li>
+            <li class="nav-item">
+            <Button variant={"unstyled"} color={"white"} style={{ marginLeft: '1rem' }}>
+                <Link to="/trending">Trending</Link>
+            </Button>
+            </li>
+            <li class="nav-item">
+            <Button variant={"unstyled"} color={"white"} style={{ marginLeft: '1rem' }}>
+                <Link to="/featured">Featured</Link>
+            </Button>
+            </li>
+          </ul>
+        </div>
+      </nav>
   </HStack>
   )
 } 
