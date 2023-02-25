27f583ec4b56d52d38ea9150af6ce09aa2f586dd
Swapnil Aichbhaumik <swapnil.aichbhaumik.cse25@heritageit.edu.in>
2023-02-25 21:04:40 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,15 @@
 import { Box, Image, Text } from "@chakra-ui/react";
 import React from "react";
-import btcSrc from "../assets/btc.png";
+import btcSrc from "../assets/btc2.jpg";
 
 const Home = () => {
   return (
     <Box bgColor={"blackAlpha.700"} w={"full"} h={"90vh"}>
-        <Image w={"full"} h={"full"} objectFit={"contain"} src={btcSrc} />
-        <Text fontSize={"5xl"} textAlign={"center"} fontWeight={"bold"} color={"whiteAlpha.700"} mt={"-20"}>Crypto'ed</Text>
+        <Image w={"full"} h={"full"} objectFit={"cover"} src={btcSrc} />
+        <Text fontSize={"5xl"} textAlign={"center"} fontWeight={"bold"} color={"whiteAlpha.900"} mt={"-20"}>Crypto'ed</Text>
     </Box>
   )
 }
 
 export default Home;
+
